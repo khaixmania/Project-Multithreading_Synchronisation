2dcb4d4ea9c58c3f559be7b36506b9ed80aa85e6
Kaan Akman <145116515+khaixmania@users.noreply.github.com>
2025-11-25 22:02:10 +0100

    Add script to plot execution time vs thread count

---
+++ Partie 1/rw_plots.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data = pd.read_csv("analyses/reader_writer.csv")
+grouped = data.groupby("thread")["time"].agg(["mean","std"]).reset_index()
+plt.figure(figsize=(8,6))
+plt.errorbar(grouped['thread'], grouped['mean'], yerr=grouped['std'], fmt='o-', capsize=5)
+plt.xticks(grouped['thread'])
+
+plt.xlabel("Nombre de threads")
+plt.ylabel("Temps d'exécution (secondes)")
+plt.title("Temps d'exécution vs Nombre de threads")
+plt.ylim(bottom=0)
+plt.grid(True)
+plt.savefig('rw_plot.pdf', dpi=300)
