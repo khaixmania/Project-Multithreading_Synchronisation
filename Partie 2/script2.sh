d203f8cc0157ae211af9dfb75c5591a687d3dc6d
Kaan Akman <145116515+khaixmania@users.noreply.github.com>
2025-11-29 14:10:01 +0100

    Add tasks for threading analysis and compilation

--- Partie 2/script2.sh
+++ Partie 2/script2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# Tâches 2.2-2.3
 THREADS=(1 2 4 8 16 32)
 FILES=("test1" "test2")
 
@@ -28,6 +28,41 @@ for programme in "${FILES[@]}"; do
                 totaltime=$(echo "$endtime - $starttime" | bc -l)
                 echo "$thread,$i,$totaltime" >> "analyses2/${programme}.csv"
             done
+        fi
+    done
+done
+
+#Tâche 2.5
+THREADS2=(2 4 8 16 32)
+FILES2=("philosophers_spin" "producer_consumer_spin" "reader_writer_spin")
+
+for programme in "${FILES2[@]}"; do
+    echo "thread,essai n°,time" > "analyses2/${programme}.csv"
+    if [ "$programme" == "philosophers_spin" ]; then
+        gcc -Wall -o "$programme" philosophers_spin.c spinlocks2.c -lpthread
+    elif [ "$programme" == "producer_consumer_spin" ]; then
+        gcc -Wall -o "$programme" producer_consumer_spin.c spinlocks2.c my_semaphore.c -lpthread
+    else
+        gcc -Wall -o "$programme" reader_writer_spin.c spinlocks2.c my_semaphore.c -lpthread
+    fi
+    for thread in "${THREADS2[@]}"; do
+	if [ "$programme" == "philosophers_spin" ]; then
+	for (( i=1; i<=5; i++)); do
+            starttime=$(date +%s.%N)
+            ./"$programme" "$thread" > /dev/null
+            endtime=$(date +%s.%N)
+            totaltime=$(echo "$endtime - $starttime" | bc -l)
+            echo "$thread,$i,$totaltime" >> "analyses2/${programme}.csv"
+        done
+        else
+        for (( i=1; i<=5; i++)); do
+            divided=$((thread/2))
+            starttime=$(date +%s.%N)
+            ./"$programme" "$divided" "$divided" > /dev/null
+            endtime=$(date +%s.%N)
+            totaltime=$(echo "$endtime - $starttime" | bc -l)
+            echo "$thread,$i,$totaltime" >> "analyses2/${programme}.csv"
+        done
         fi
     done
 done
