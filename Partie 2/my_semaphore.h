9f05b6452c71d650fd98d2e2a9891175d459fbf2
Kaan Akman <145116515+khaixmania@users.noreply.github.com>
2025-11-29 13:46:36 +0100

    Create my_semaphore.h

---
+++ Partie 2/my_semaphore.h
@@ -0,0 +1,13 @@
+#include "spinlocks2.h" //Choix de la meilleure logique (test-and-test-and-set)
+typedef struct {
+    volatile int x; //compteur pour le nbr de ressources dispo
+    int verrou;
+} my_sem_t;
+//init sémpahore avec une valeur
+void my_sem_init(my_sem_t *sem, int val);
+
+//remplacer sem_wait, decremente x
+void my_sem_wait(my_sem_t *sem);
+
+//remplacer sem_post, incremente x
+void my_sem_post(my_sem_t *sem);
