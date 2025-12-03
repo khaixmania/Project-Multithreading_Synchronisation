3d3f341aed39c384b4e234b568299723d4d5bd88
Kaan Akman <145116515+khaixmania@users.noreply.github.com>
2025-12-03 20:23:19 +0100

    Update script.sh

--- Partie 1/script.sh
+++ Partie 1/script.sh
@@ -8,14 +8,14 @@ for programme in "${FILES[@]}";
     do
     echo "thread,essai n°,time" > "analyses/${programme}.csv"
     gcc -Wall -o "$programme" "$programme".c -lpthread
-    for thread in "${THREADS[@]}"; do
+    for thread in "${THREADS[@]}"; do # '@' permet d'itérer sur chaque élément dans THREADS
 	if [ "$programme" == "philosophers" ];
 	then
 	for (( i=1; i<=5; i++)); do
-	    starttime=$(date +%s.%N)
-	    ./"$programme" "$thread" > /dev/null
+	    starttime=$(date +%s.%N) #%s: c'est les secondes depuis 01/01/1970 et %N: nanosecondes pour plus de précision
+	    ./"$programme" "$thread" > /dev/null #stdout vers rien pour pas nuire nos perfs
 	    endtime=$(date +%s.%N)
-	    totaltime=$(echo "$endtime - $starttime" | bc -l)
+	    totaltime=$(echo "$endtime - $starttime" | bc -l) #bc: basic calculator est là avec -l pour calculer les float sans perte
 	    echo "$thread,$i,$totaltime" >> "analyses/${programme}.csv"
 	done
 	else
